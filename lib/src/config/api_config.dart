966c30100c049dba612a621dd2e6fd90fd93e201
monykappa <kimmony039@gmail.com>
2026-05-05 20:34:25 +0700

    feat: update for client delivery

--- lib/src/config/api_config.dart
+++ lib/src/config/api_config.dart
@@ -1,11 +1,10 @@
 class ApiConfig {
   static const String baseUrl = 'https://face.chlat.dev';
 
-  // If you already have a working `Authorization` header from FastAPI,
+  // If  already have a working `Authorization` header from FastAPI,
   // set it here (for example: Basic xxxxxx). Leave empty to use username/password.
   static const String basicAuthHeaderOverride = '';
 
-  // Replace with your actual credentials when override is empty.
   static const String basicAuthUsername = 'adminFacebioAuth';
   static const String basicAuthPassword = 'bioFaceacleda\$121120\$';
 }
